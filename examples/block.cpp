427e94699d7f60da2484d45dc4439331e62e6554
Victor Bucataru <ping@devktor.com>
2015-10-02 23:57:16 +0300

    fetching uncles example

--- examples/block.cpp
+++ examples/block.cpp
@@ -10,8 +10,14 @@ int main()
 {
     Provider provider("ipc://home/vic/.ethereum/geth.ipc");
     BlockChain blockchain(provider);
-    Block block = blockchain.getBlock(1);
+    Block block = blockchain.getBlock(94);
     std::cout<<"hash = "<<block.getHash()<<"\n";
-
+    Collection<std::string> uncles = block.getUncles();
+    std::cout<<"uncles: ";
+    for(Collection<std::string>::Iterator it = uncles.begin(), end = uncles.end(); it!= end; ++it)
+    {
+        std::cout<<*it<<"  ";
+    }
+    std::cout<<"\n";
     return 0;
 }
