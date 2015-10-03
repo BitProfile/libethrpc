f355bc79f5ce59e95698dfb1dda9470670eb733e
Victor Bucataru <ping@devktor.com>
2015-10-04 01:39:00 +0300

    transaction example

---
+++ examples/transaction.cpp
@@ -0,0 +1,24 @@
+
+#include <iostream>
+
+#include "BlockChain.hpp"
+#include "Provider.hpp"
+
+using namespace Eth;
+
+int main()
+{
+    Provider provider("ipc://home/vic/.ethereum/geth.ipc");
+    BlockChain blockchain(provider);
+    Transaction transaction = blockchain.getBlockTransaction(329513, 1);
+
+    std::cout<<"hash : "<<transaction.getHash()<<std::endl;
+    std::cout<<"nonce : "<<transaction.getNonce()<<std::endl;
+    std::cout<<"from : "<<transaction.getSender()<<std::endl;
+    std::cout<<"to : "<<transaction.getReceiver()<<std::endl;
+    std::cout<<"amount : "<<transaction.getAmount()<<std::endl;
+    std::cout<<"gas : "<<transaction.getGas()<<std::endl;
+    std::cout<<"gas price : "<<transaction.getGasPrice()<<std::endl;
+
+    return 0;
+}
