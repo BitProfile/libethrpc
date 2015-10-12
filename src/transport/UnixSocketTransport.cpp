3cb50a699415675f8708574f92136f54d2adb8b7
Victor Bucataru <ping@devktor.com>
2015-10-12 21:16:49 +0300

    close already opened connection

--- src/transport/UnixSocketTransport.cpp
+++ src/transport/UnixSocketTransport.cpp
@@ -8,6 +8,12 @@ bool UnixSocketConnector::connect(UnixSocket &socket, const char *path)
 {
     UnixEndpoint endpoint(path);
     boost::system::error_code ec;
+
+    if(socket.is_open())
+    {
+        socket.close();
+    }
+
     socket.connect(endpoint, ec);
 
     if(ec)
