637da7b770d5cc7b8bbbb7b4101abb4bb607970d
Victor Bucataru <ping@devktor.com>
2015-10-12 21:10:23 +0300

    log connection error

--- src/transport/UnixSocketTransport.cpp
+++ src/transport/UnixSocketTransport.cpp
@@ -9,7 +9,15 @@ bool UnixSocketConnector::connect(UnixSocket &socket, const char *path)
     UnixEndpoint endpoint(path);
     boost::system::error_code ec;
     socket.connect(endpoint, ec);
-    return !ec;
+
+    if(ec)
+    {
+        LOG_DEBUG("connection to "<<path<<" failed : "<<ec);
+
+        return false;
+    }
+
+    return true;
 }
 
 
