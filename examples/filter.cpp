cdf20cf7f5f525b4ad5e29a096d45ba6284a981b
Victor Bucataru <ping@devktor.com>
2015-10-21 00:07:29 +0300

    using FilterLog

--- examples/filter.cpp
+++ examples/filter.cpp
@@ -9,14 +9,15 @@ int main()
     Provider provider("ipc://home/vic/.ethereum/geth.ipc");
     BlockChain blockchain(provider);
 
-    uint64_t id = blockchain.setNewFilter("0x580992b51e3925e23280efb93d3047c82f17e038");
+    uint64_t id = blockchain.setNewFilter("0x1194e966965418c7d73a42cceeb254d875860356", 1);
     std::cout<<"filter id "<<id<<"\n";
-    
-    Collection<TransactionEvent> events = blockchain.getEvents(id);
+
+    sleep(2);
+    Collection<FilterLog> events = blockchain.getFilterChanges(id);
     
     std::cout<<"events ("<<events.size()<<") : \n";
     
-    for(Collection<TransactionEvent>::Iterator it = events.begin(), end= events.end(); it!= end; ++it)
+    for(Collection<FilterLog>::Iterator it = events.begin(), end= events.end(); it!= end; ++it)
     {
         std::cout<<"type: "<<it->getType()<<", transaction "<<it->getTransactionHash()<<" ("<<it->getTransactionIndex()<<") "<<" block "<<it->getBlockHash()<<" ("<<it->getBlockIndex()<<") \n";
     }
