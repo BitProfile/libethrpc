593a6cec1ee004e6223109030be81bc611f6c025
Victor Bucataru <ping@devktor.com>
2015-11-04 01:35:45 +0200

    solidity compiler example params

--- examples/solidity_compiler.cpp
+++ examples/solidity_compiler.cpp
@@ -8,7 +8,8 @@ using namespace Ethereum::Connector;
 
 int main()
 {
-    Provider provider("ipc://home/vic/.ethereum/geth.ipc");
+    DefaultGethPath path;
+    Provider provider(path);
     Compiler compiler(provider);
     std::string code = compiler.compileSolidity("contract test { function multiply(uint a) returns(uint d) {   return a * 7;   } }");
     std::cout<<"code: "<<code<<std::endl;
