8c78322fdc41d5bde0f37f15849925b755521a72
Victor Bucataru <ping@devktor.com>
2015-11-03 02:00:32 +0200

    wallet example

---
+++ examples/wallet.cpp
@@ -0,0 +1,21 @@
+
+#include <iostream>
+
+#include "Wallet.hpp"
+#include "Provider.hpp"
+
+using namespace Eth;
+
+int main()
+{
+    Provider provider("ipc://home/vic/.ethereum/geth.ipc");
+    Wallet wallet(provider);
+    Collection<std::string> accounts = wallet.getAccounts();
+    for(Collection<std::string>::Iterator it=accounts.begin(); it!=accounts.end(); ++it)
+    {
+        std::cout<<"account : "<<*it<<std::endl;
+    }
+    
+
+    return 0;
+}
