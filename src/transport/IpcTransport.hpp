c3ad207f49445279a2f92c05d26d316c512c8d6e
Victor Bucataru <ping@devktor.com>
2016-02-29 14:05:05 +0200

    typedef

--- src/transport/IpcTransport.hpp
+++ src/transport/IpcTransport.hpp
@@ -2,13 +2,13 @@
 
 #include <boost/asio.hpp>
 
+
+
 #if defined(BOOST_ASIO_HAS_LOCAL_SOCKETS)
     #include "UnixSocketTransport.hpp"
-    #define IPC_TRANSPORT_CLASS_NAME UnixSocketTransport
 #else
     #if defined(__WINDOWS_OS__)
         #include "NamedPipeTransport.hpp"
-        #define IPC_TRANSPORT_CLASS_NAME NamedPipeTransport
     #else
         #error Local sockets not available on this platform.
     #endif
@@ -16,7 +16,13 @@
 
 namespace Ethereum{namespace Connector{
 
-typedef IPC_TRANSPORT_CLASS_NAME IpcTransport;
+#if defined(BOOST_ASIO_HAS_LOCAL_SOCKETS)
+    typedef UnixSocketTransport IpcTransport;
+#else
+    typedef NamedPipeTransport IpcTransport;
+#endif
+
+
 
 
 }}
