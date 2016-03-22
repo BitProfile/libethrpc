c08f11edf94d20cec2803a37252afe6d1f7b789a
Victor Bucataru <ping@devktor.com>
2016-03-22 23:55:31 +0200

    merging libabi

--- cmake/lib.cmake
+++ cmake/lib.cmake
@@ -1,5 +1,14 @@
 find_package(JsonCPP REQUIRED)
 find_package(Boost COMPONENTS system thread REQUIRED)
+find_package(CryptoPP)
+
+
+if(CRYPTOPP_INCLUDE_DIR)
+    set(ABI_ENALED TRUE)
+    add_definitions(-D__ABI_ENABLED__)
+    add_subdirectory(${CMAKE_CURRENT_SOURCE_DIR}/abi)
+endif()
+
 
 include(${CMAKE_CURRENT_SOURCE_DIR}/cmake/modules/CopyHeaders.cmake)
 
@@ -25,8 +34,37 @@ endif()
 
 CopyHeaders()
 
+if(ABI_ENALED)
+    include_directories(${CMAKE_CURRENT_BINARY_DIR}/abi/include)
+endif()
+
 
 add_library(ethrpc STATIC ${SOURCES})
+
+if(ABI_ENALED)
+    add_dependencies(ethrpc abi)
+
+    if (WIN32 AND NOT MINGW)
+        set_property (TARGET ethrpc APPEND PROPERTY STATIC_LIBRARY_FLAGS $<TARGET_OBJECTS:abi>)
+    elseif (${CMAKE_GENERATOR} STREQUAL "Xcode")
+        set_property (TARGET ethrpc APPEND PROPERTY STATIC_LIBRARY_FLAGS $<TARGET_OBJECTS:abi>)
+    else()
+        set(LIB_OBJ_DIR ${CMAKE_CURRENT_BINARY_DIR}/obj)
+        set(ETHRPC_LIBRARY_PATH $<TARGET_FILE:ethrpc>)
+        set(ABI_LIBRARY_PATH $<TARGET_FILE:abi>)
+        make_directory (${LIB_OBJ_DIR})
+
+        ADD_CUSTOM_COMMAND(TARGET ethrpc
+              POST_BUILD
+              COMMAND echo "packing libethrpc.a"
+              COMMAND ${CMAKE_AR} x  ${ABI_LIBRARY_PATH} WORKING_DIRECTORY ${LIB_OBJ_DIR}
+              COMMAND ${CMAKE_AR} x  ${ETHRPC_LIBRARY_PATH} WORKING_DIRECTORY ${LIB_OBJ_DIR}
+              COMMAND ${CMAKE_AR} rcs  ${ETHRPC_LIBRARY_PATH} ${LIB_OBJ_DIR}/*.o
+              COMMAND rm -fr ${LIB_OBJ_DIR}
+        )
+    endif ()
+    file(COPY ${CMAKE_CURRENT_BINARY_DIR}/abi/include/abi DESTINATION ${CMAKE_CURRENT_BINARY_DIR}/include/ethrpc)
+endif()
 
 install(DIRECTORY ${PROJECT_BINARY_DIR}/include/ethrpc DESTINATION include)
 install (TARGETS ethrpc ARCHIVE DESTINATION lib LIBRARY DESTINATION lib RUNTIME DESTINATION bin)
