530bda0f634ae7b014814206c6ece14121e924e3
Victor Bucataru <ping@devktor.com>
2016-03-22 23:55:57 +0200

    contract class

---
+++ src/Contract.cpp
@@ -0,0 +1,52 @@
+#include "Contract.hpp"
+
+#ifdef __ABI_ENABLED__
+
+namespace Ethereum{namespace Connector{
+
+
+Contract::Contract(Provider &provider, const std::string &address) : 
+    Base(address, ContractInvoker(provider))
+{}
+
+
+
+std::string ContractInvoker::execute(const std::string &to, const std::string &code)
+{
+    return execute(getDefaultAddress(), to, code);
+}
+
+
+std::string ContractInvoker::execute(const std::string &from, const std::string &to, const std::string &code)
+{
+    GasEstimator estimator(_provider);
+    BigInt gas = estimator.estimate(from, to, BigInt(0), code);
+    Json::Value result = _provider.request("eth_sendTransaction", TransactionParamsFactory::makeParams(from.c_str(), to.c_str(), BigInt(0), code.c_str(), gas, 0));
+    return result.asString();
+}
+
+
+std::string ContractInvoker::call(const std::string &to, const std::string &code)
+{
+    return call(getDefaultAddress(), to, code);
+}
+
+
+std::string ContractInvoker::call(const std::string &from, const std::string &to, const std::string &code)
+{
+    Json::Value result = _provider.request("eth_call", TransactionParamsFactory::makeParams(from.c_str(), to.c_str(), BigInt(0), code.c_str(), 0));
+    return result.asString();
+}
+
+
+std::string ContractInvoker::getDefaultAddress()
+{
+    Json::Value result = _provider.request("eth_coinbase");
+    return result.asString();
+}
+
+
+
+}}
+
+#endif
---
+++ src/Contract.hpp
@@ -0,0 +1,53 @@
+#pragma once 
+
+#ifdef __ABI_ENABLED__
+
+#include <string>
+
+#include "abi/Reflection.hpp"
+#include "Provider.hpp"
+#include "GasEstimator.hpp"
+
+#include "detail/TransactionParamsFactory.hpp"
+
+
+namespace Ethereum{namespace Connector{
+
+
+class ContractInvoker
+{
+    public:
+        ContractInvoker(Provider &);
+        std::string call(const std::string &, const std::string &);
+        std::string call(const std::string &, const std::string &, const std::string &);
+        std::string execute(const std::string &, const std::string &);
+        std::string execute(const std::string &, const std::string &, const std::string &);
+
+    private:
+        std::string getDefaultAddress();
+
+    private:
+        Provider &_provider;
+};
+
+
+#define ContractArguments Ethereum::ABI::ARGUMENTS
+
+
+typedef Ethereum::ABI::Result ContractResult;
+
+
+class Contract : public Ethereum::ABI::Reflection<ContractInvoker>
+{
+    public:
+        typedef Ethereum::ABI::Reflection<ContractInvoker> Base;
+
+    public:
+        Contract(Provider &, const std::string &address);
+
+};
+
+
+}}
+
+#endif
