575c1aea75192fb6e8d5156bb4e4ba963a75ffd9
Victor Bucataru <ping@devktor.com>
2016-03-23 20:05:38 +0200

    using GLOB_RECURSE for library sources

--- cmake/lib.cmake
+++ cmake/lib.cmake
@@ -6,6 +6,7 @@ find_package(CryptoPP)
 if(CRYPTOPP_INCLUDE_DIR)
     set(ABI_ENALED TRUE)
     add_definitions(-D__ABI_ENABLED__)
+    add_definitions(-DBOOST_PP_VARIADICS)
     add_subdirectory(${CMAKE_CURRENT_SOURCE_DIR}/abi)
 endif()
 
@@ -15,10 +16,7 @@ include(${CMAKE_CURRENT_SOURCE_DIR}/cmake/modules/CopyHeaders.cmake)
 include_directories(${Boost_INCLUDE_DIRS} ${CMAKE_CURRENT_SOURCE_DIR}/src)
 include_directories(${JSONCPP_INCLUDE_DIR})
 
-file(GLOB SOURCES 
-    "src/*.cpp"
-    "src/transport/*.cpp"
-)
+file(GLOB_RECURSE SOURCES "src/*.cpp")
 
 include(CheckTypeSize) 
 
@@ -60,7 +58,6 @@ if(ABI_ENALED)
               COMMAND ${CMAKE_AR} x  ${ABI_LIBRARY_PATH} WORKING_DIRECTORY ${LIB_OBJ_DIR}
               COMMAND ${CMAKE_AR} x  ${ETHRPC_LIBRARY_PATH} WORKING_DIRECTORY ${LIB_OBJ_DIR}
               COMMAND ${CMAKE_AR} rcs  ${ETHRPC_LIBRARY_PATH} ${LIB_OBJ_DIR}/*.o
-              COMMAND rm -fr ${LIB_OBJ_DIR}
         )
     endif ()
     file(COPY ${CMAKE_CURRENT_BINARY_DIR}/abi/include/abi DESTINATION ${CMAKE_CURRENT_BINARY_DIR}/include/ethrpc)
