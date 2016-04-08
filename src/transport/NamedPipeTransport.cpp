b0204aeca2074ef02d9876dd6cd8f808c585ca66
Victor Bucataru <ping@devktor.com>
2016-04-09 00:55:47 +0300

    fixed named pipe connect error detection

--- src/transport/NamedPipeTransport.cpp
+++ src/transport/NamedPipeTransport.cpp
@@ -8,7 +8,7 @@ namespace Ethereum{namespace Connector{
 bool NamedPipeConnector::connect(NamedPipeSocket &socket, const char *path)
 {
     HANDLE pipe = CreateFile(path, GENERIC_READ|GENERIC_WRITE, 0, NULL, OPEN_EXISTING, FILE_FLAG_OVERLAPPED, NULL);
-    if(!pipe)
+    if(pipe==INVALID_HANDLE_VALUE)
     {
         return false;
     }
